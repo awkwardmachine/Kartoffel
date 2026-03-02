66033f41a74dc9bad1d88e9cc0318a4376bd8655
awkwardmachine <noib2009@gmail.com>
2026-03-02 19:17:50 +0000

    Moved to 3D space and added Camera class and basic Input Manager class implementation

---
+++ Camera.hpp
@@ -0,0 +1,38 @@
+//
+// Created by nil on 02/03/2026.
+//
+
+#ifndef KARTOFFEL_CAMERA_HPP
+#define KARTOFFEL_CAMERA_HPP
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera {
+public:
+    Camera();
+
+    [[nodiscard]] glm::mat4        GetViewMatrix() const;
+    [[nodiscard]] static glm::mat4 GetProjectionMatrix(float aspect_ratio);
+
+    void SetPosition(float x, float y, float z);
+    void Move(float forward, float right);
+
+private:
+    glm::vec3 position_;
+    glm::vec3 front_;
+    glm::vec3 up_;
+    glm::vec3 right_{};
+    glm::vec3 world_up_;
+
+    float yaw_;
+    float pitch_;
+
+    float movement_speed_;
+    float mouse_sensitivity_;
+
+    void UpdateVectors();
+};
+
+
+#endif // KARTOFFEL_CAMERA_HPP
--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
+#include <glad/glad.h>
 #include <iostream>
 
+#include "Camera.hpp"
+#include "InputManager.hpp"
 #include "Mesh.hpp"
 #include "Renderer.hpp"
 #include "Shader.hpp"
 #include "Window.hpp"
 
 int main() {
-    Window window;
-    Shader shader;
-    Mesh   triangle;
+    Window       window;
+    Shader       shader;
+    Mesh         triangle;
+    Camera       camera;
+    InputManager input;
 
     if (!window.Initialize(800, 600, "Kartoffel")) {
         std::cerr << "Failed to initialize window" << std::endl;
@@ -20,6 +25,8 @@ int main() {
         return -1;
     }
 
+    input.Initialize(window.GetNativeWindow());
+
     if (!shader.LoadFromFile("Shaders/triangle.vert", "Shaders/triangle.frag")) {
         std::cerr << "Failed to load Shaders" << std::endl;
         return -1;
@@ -30,11 +37,38 @@ int main() {
         return -1;
     }
 
+    camera.SetPosition(0.0f, 0.0f, 3.0f);
+    auto model = glm::mat4(1.0f); // position triangle in world space
+
     while (!window.ShouldClose()) {
         Window::PollEvents();
+        input.Update();
+
+        if (input.IsKeyPressed(GLFW_KEY_W))
+            camera.Move(0.015f, 0.0f);
+        if (input.IsKeyPressed(GLFW_KEY_S))
+            camera.Move(-0.015f, 0.0f);
+        if (input.IsKeyPressed(GLFW_KEY_A))
+            camera.Move(0.0f, -0.015f);
+        if (input.IsKeyPressed(GLFW_KEY_D))
+            camera.Move(0.0f, 0.015f);
+
         Renderer::Clear(0.2f, 0.3f, 0.3f, 1.0f);
 
+        const float aspect_ratio = static_cast<float>(window.GetWidth()) / static_cast<float>(window.GetHeight());
+        glm::mat4   view         = camera.GetViewMatrix();
+        glm::mat4   projection   = Camera::GetProjectionMatrix(aspect_ratio);
+
         shader.Use();
+
+        const GLint model_loc = glGetUniformLocation(shader.GetId(), "model");
+        const GLint view_loc  = glGetUniformLocation(shader.GetId(), "view");
+        const GLint proj_loc  = glGetUniformLocation(shader.GetId(), "projection");
+
+        glUniformMatrix4fv(model_loc, 1, GL_FALSE, &model[0][0]);
+        glUniformMatrix4fv(view_loc, 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv(proj_loc, 1, GL_FALSE, &projection[0][0]);
+
         triangle.Render();
 
         window.SwapBuffers();
